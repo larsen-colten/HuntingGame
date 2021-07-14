6e38a5427db83b8bcd472df84a7c9a504c141141
Colten Larsen <larsen.colten@gmail.com>
2021-07-13 20:36:19 -0600

    main menu

---
+++ Assets/Scripts/HealthDisplay.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour
+{
+    private int health = 5;
+    public Text healthText;
+
+    void Update(){
+        healthText.text = "HEALTH : " + health;
+
+        if (Input.GetKeyDown(KeyCode.P)){
+            health--;
+        }
+    }
+}
